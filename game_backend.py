3c2fdc42374f46a2daf7b436550f42061969cae7
MarkVHGN <mverhagen@ups.com>
2026-06-19 12:03:28 +0200

    Simplify to 3 options: 2 AI + 1 human answer per question

--- game_backend.py
+++ game_backend.py
@@ -195,8 +195,8 @@ class QuizHandler(BaseHTTPRequestHandler):
         question = QUESTIONS[question_num]
         human_answer = game["human_answers"][str(question_num)]
         
-        # Get 3 random AI answers
-        ai_options = random.sample(AI_RESPONSES[question], 3)
+        # Get 2 random AI answers
+        ai_options = random.sample(AI_RESPONSES[question], 2)
         
         # Create options list with human answer in random position
         options = ai_options + [human_answer]
@@ -204,7 +204,7 @@ class QuizHandler(BaseHTTPRequestHandler):
         
         # Find the correct answer position
         correct_position = options.index(human_answer)
-        correct_letter = chr(65 + correct_position)  # A, B, C, or D
+        correct_letter = chr(65 + correct_position)  # A, B, or C
         
         # Store for checking later
         game["current_question"] = {
@@ -219,8 +219,7 @@ class QuizHandler(BaseHTTPRequestHandler):
             "options": {
                 "A": options[0],
                 "B": options[1],
-                "C": options[2],
-                "D": options[3]
+                "C": options[2]
             }
         })
 
